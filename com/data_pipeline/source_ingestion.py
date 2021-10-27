f4c3adae1d49f6f79af437e628f782f0203ee00f
Pritam-Mohanty-Code <pritammohanty@hotmail.com>
2021-10-26 22:03:08 -0400

    initial commit

--- com/data_pipeline/source_ingestion.py
+++ com/data_pipeline/source_ingestion.py
@@ -1,49 +1,104 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import utils.utilities as ut
 import yaml
 import os.path
-import utils.aws_utils as ut
+from pyspark.sql.types import StructType, IntegerType, BooleanType, DoubleType
 
 if __name__ == '__main__':
-
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
-    )
-
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read ingestion enterprise applications") \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
-    jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
-                  "lowerBound": "1",
-                  "upperBound": "100",
-                  "dbtable": app_conf["mysql_conf"]["dbtable"],
-                  "numPartitions": "2",
-                  "partitionColumn": app_conf["mysql_conf"]["partition_column"],
-                  "user": app_secret["mysql_conf"]["username"],
-                  "password": app_secret["mysql_conf"]["password"]
-                   }
-    # print(jdbcParams)
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("DataFrames examples") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
 
-    # use the ** operator/un-packer to treat a python dictionary as **kwargs
-    print("\nReading data from MySQL DB using SparkSession.read.format(),")
-    txnDF = spark\
-        .read.format("jdbc")\
-        .option("driver", "com.mysql.cj.jdbc.Driver")\
-        .options(**jdbc_params)\
-        .load()
+    # Setup spark to use s3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
-    txnDF.show()
+    src_list = app_conf["source_list"]
+    # Check if passed from cmd line arg then override the above (e.g. source_list=OL,SB)
+    for src in src_list:
+        output_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src
+        src_conf = app_conf[src]
+        if src == 'OL':
+            print('Read loyalty data from SFTP folder and write it to S3 bucket')
+            ol_txn_df = spark.read\
+                .format("com.springml.spark.sftp")\
+                .option("host", app_secret["sftp_conf"]["hostname"])\
+                .option("port", app_secret["sftp_conf"]["port"])\
+                .option("username", app_secret["sftp_conf"]["username"])\
+                .option("pem", os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"]))\
+                .option("fileType", "csv")\
+                .option("delimiter", "|")\
+                .load(src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
+            ol_txn_df = ol_txn_df.withColumn("ins_dt", current_date())
+            ol_txn_df.show(5, False)
+            ut.write_to_s3(ol_txn_df, output_path)
+
+        elif src == 'SB':
+            print('Read sales data from MySQL db and write it to S3 bucket')
+            jdbc_params = {
+                "url": ut.get_mysql_jdbc_url(app_secret),
+                "lowerBound": "1",
+                "upperBound": "100",
+                "dbtable": src_conf["mysql_conf"]["dbtable"],
+                "numPartitions": "2",
+                "partitionColumn": src_conf["mysql_conf"]["partition_column"],
+                "user": app_secret["mysql_conf"]["username"],
+                "password": app_secret["mysql_conf"]["password"]
+                           }
+
+            print("\nReading data from MySQL DB using SparkSession.read.format(),")
+            txn_df = spark\
+                .read.format("jdbc")\
+                .option("driver", "com.mysql.cj.jdbc.Driver")\
+                .options(**jdbc_params)\
+                .load()
+            txn_df = txn_df.withColumn("ins_dt", current_date())
+            txn_df.show()
+            ut.write_to_s3(txn_df, output_path)
+
+        elif src == 'CP':
+            print("\nReading customers data from S3 and write it to s3") # kc_extract.. KC_Extract_1_20171009
+            cp_df = spark.read \
+                .option("mode", "DROPMALFORMED") \
+                .option("delimiter", "|") \
+                .option('header', 'true')\
+                .option("inferSchema", "true") \
+                .csv("s3a://" + src_conf["s3_conf"]["s3_bucket"] + "/" + src_conf["s3_conf"]["filename"])
+            cp_df = cp_df.withColumn("ins_dt", current_date())
+            ut.write_to_s3(cp_df, output_path)
+
+        elif src == 'ADDR':
+            cust_addr = spark\
+                .read\
+                .format("com.mongodb.spark.sql.DefaultSource")\
+                .option("database", src_conf["mongodb_config"]["database"])\
+                .option("collection", src_conf["mongodb_config"]["collection"])\
+                .load()
+            cust_addr = cust_addr\
+                .select(col("consumer_id"),
+                        col("mobile-no").alias("mobile"),
+                        col("address.street").alias("address"),
+                        col("address.city").alias("city"),
+                        col("address.state").alias("state")
+                        )
+            # "consumer_id":"I034867789V","address":{"street":"aca","city":"bangalore","state":"karnataka"},"mobile-no":"7789327282"}
+            cust_addr = cust_addr.withColumn("ins_dt", current_date())
+            cust_addr.show()
+            ut.write_to_s3(cust_addr, output_path)
 
 # spark-submit --packages "mysql:mysql-connector-java:8.0.15" dataframe/ingestion/others/systems/mysql_df.py
---
+++ utils/utilities.py
@@ -0,0 +1,43 @@
+def get_redshift_jdbc_url(redshift_config: dict):
+    host = redshift_config["redshift_conf"]["host"]
+    port = redshift_config["redshift_conf"]["port"]
+    database = redshift_config["redshift_conf"]["database"]
+    username = redshift_config["redshift_conf"]["username"]
+    password = redshift_config["redshift_conf"]["password"]
+    return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host, port, database, username, password)
+
+
+def get_mysql_jdbc_url(mysql_config: dict):
+    host = mysql_config["mysql_conf"]["hostname"]
+    port = mysql_config["mysql_conf"]["port"]
+    database = mysql_config["mysql_conf"]["database"]
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
+
+
+def write_to_s3(df, path):
+    print('Writing data to', path)
+    df.write \
+        .mode("overwrite") \
+        .partitionBy("ins_dt") \
+        .parquet(path)
+
+
+def read_from_redshift(spark, jdbc_url, s3_temp_path, query):
+    return spark.read \
+        .format("io.github.spark_redshift_community.spark.redshift") \
+        .option("url", jdbc_url) \
+        .option("query", query) \
+        .option("forward_spark_s3_credentials", "true") \
+        .option("tempdir", s3_temp_path) \
+        .load()
+
+
+def write_to_redshift(df, jdbc_url, s3_temp_path, table_name):
+    df.write \
+        .format("io.github.spark_redshift_community.spark.redshift") \
+        .option("url", jdbc_url) \
+        .option("tempdir", s3_temp_path) \
+        .option("forward_spark_s3_credentials", "true") \
+        .option("dbtable", table_name) \
+        .mode("append") \
+        .save()
