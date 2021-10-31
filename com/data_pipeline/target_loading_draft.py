c00dc7f6a5076d994b831de5c6ee7066b6fd41cd
Pritam-Mohanty-Code <pritammohanty@hotmail.com>
2021-10-31 14:12:54 -0400

    initial commit

--- com/data_pipeline/target_loading_draft.py
+++ com/data_pipeline/target_loading_draft.py
@@ -1,24 +1,11 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import utils.functions as ut
 import yaml
 import os.path
-import sys
-import utils.aws_utils as ut
+from pyspark.sql.types import StructType, IntegerType, BooleanType,DoubleType
 
 if __name__ == '__main__':
-
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar"\
-         --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
-    )
-
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read ingestion enterprise applications") \
-        .master('local[*]') \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
@@ -28,16 +15,33 @@ if __name__ == '__main__':
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("DataFrames examples") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
     # Setup spark to use s3
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
-    # Read CP and ADDR data for current data
-    # create temp view on top of that
-    # execute a spark sql query to get the dimension table data
+    src_list = app_conf['source_list']
 
+    for src in src_list:
+        src_conf = app_conf[src]
 
-    print("Completed   <<<<<<<<<")
+    cp_df = ut.read.from_s3(spark, src_conf)
 
-# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/provision/df_redshift.py
+    spark.sql("""
+                SELECT
+               DISTINCT REGIS_CNSM_ID, CAST(REGIS_CTY_CODE AS SMALLINT), CAST(REGIS_ID AS INTEGER),
+               REGIS_LTY_ID, REGIS_DATE, REGIS_CHANNEL, REGIS_GENDER, REGIS_CITY, INS_DT
+             FROM
+               CP
+             WHERE
+             INS_DT = CURRENT_DATE
+             """) \
+        .show(5, False)
